6d96aeffc7fa3228bab7b5049b81f513075ea8d6
s19013 <s19013@std.it-college.ac.jp>
2020-01-28 00:59:42 +0900

    1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const url = "https://illustswitch.com/wp-json/wp/v2/posts"
+
+const A = () =>
+  fetch(Movie_api_url)
+        .then(response => response.json())
+
 
 class App {
   constructor() {
-
   }
-  
+
+  render(
+    return(
+
+    )
+  )
 }
 
 export default App;
